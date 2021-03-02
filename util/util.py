b72d86e508501259d2061b5924eec2e3bb84a758
Warren Fletcher <me@fletch.band>
2021-03-02 21:47:22 +0100

    Working on creating a skelton structure. Going to design properly

--- util/util.py
+++ util/util.py
@@ -1,26 +1,42 @@
+class key_quality:
+	def __init__(self, name, intervals):
+		self.name = name
+		self.intervals = intervals
+
+class key:
+	def __init__(self,root, key_quality):
+		self.modes = []
+		self.root = root
+		self.
 
 class chord_quality:
 	def __init__(self, name, intervals):
 		self.name = name
 		self.intervals = intervals
+
 	def __len__(self):
 		return len(intervals)
 
+	def getIntervals(self):
+		return self.intervals
+
 class chord:
-	def __init__(self, root, chord_quality, basic=True):
+	def __init__(self, root, chord_quality):
 		self.root = root
 		self.chord_quality = chord_quality
 		self.length = len(chord_quality)
-	
+		self.spelling = self.buildSpelling()
+
+	def buildSpelling(self):
+		recipe = self.chord_quality.getIntervals()
+		root = self.root
 	def __len__(self):
 		return self.length
-	def get_component(self, component)
+
+	def get_component(self, component):
 
 
 class note:
-	def __init__(self, value, enharmonic):
+	def __init__(self, value, modifier=None):
 		self.value = value
-		self.enharmonic = enharmonic
-
-	def getEnharmonic(self):
-		return self.enharmonic
+		self.modifier = modifier
